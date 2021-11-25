330469ad7f5960fd27e689b20a7db88feab2d91f
Geraldalex <alex1989chekan@gmail.com>
2021-11-25 21:25:19 +0200

    firstCom

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+import * as React from 'react'
+
+import { NavigationContainer} from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import {
+    
+    PlantDetail
+} from './src/screens/'
+
+import Tabs from './src/navigation/tabs'
+
+
+const Stack = createNativeStackNavigator()
+
+const App = () => {
+  return(
+    <NavigationContainer >
+      <Stack.Navigator
+      screenOptions={{
+          headerShown:false
+      }}
+      initialRouteName={'Tabs'}
+      >
+        <Stack.Screen  name='Tabs' component={Tabs} />
+
+        <Stack.Screen  name='PlantDetail' component={PlantDetail} options={{headerShown:false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => {
+  return <App/>
+}
